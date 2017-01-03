55bd2f891a0f89edc24c8b1261c2ab3d518019b5
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2017-01-03 15:33:16 -0800

    repo setup and some tests created for stack implementation

--- spec/stack.js
+++ spec/stack.js
@@ -15,8 +15,49 @@ describe('Stack', () => {
     it('pushes an element to the top of the stack.', () => {
       const myStack = new Stack()
 
-      expect(() => myStack.push('foo'))
-        .to.alter(() => myStack.length(), { from: 0, to: 1 })
+      expect( myStack.push('foo') )
+        .to.eql( [ 'foo' ] )
+    })
+  })
+
+  context('pop()', () => {
+    it('pops an element off the top of the stack.', () => {
+      const myStack = new Stack()
+      myStack.push( 'foo' )
+      expect( myStack.pop() )
+        .to.eql( [] )
+    })
+  })
+
+  context('length()', () => {
+    it('returns number of elements in array.', () => {
+      const myStack = new Stack()
+      myStack.push( 'foo' )
+      myStack.push( 'fooBar' )
+      expect( myStack.length() )
+        .to.eql( 2 )
+    })
+  })
+
+  context('isEmpty()', () => {
+    it('returns a boolean stating whether there are elements in the array', () => {
+      const myStack = new Stack()
+      expect( myStack.isEmpty() ).to.eql( false )
+
+      myStack.push( 'foo' )
+
+      expect( myStack.isEmpty() ).to.eql( true )
+    })
+  })
+
+  context('peek()', () => {
+    it('returns top element in the stack, otherwise return null', () => {
+      const myStack = new Stack()
+      expect( myStack.peek() ).to.eql( null )
+
+      myStack.push( 'foo' )
+      console.log(myStack.peek())
+      expect( myStack.peek() ).to.eql( 'foo' )
     })
   })
 })
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,45 @@
 'use strict'
 
 export default class Stack {
-  // your code here
+  constructor( data ) {
+    this.top = 0
+    this.data = data
+    this.element = []
+  }
+
+  push( data ) {
+    console.log( this.top, data )
+    this.element[ this.top++ ] = data
+    return this.element
+  }
+
+  pop() {
+    if( this.top === 0 ){
+      return null
+    }else{
+      this.element.splice( this.element[ this.top ] )
+      return this.element
+    }
+  }
+
+  length() {
+    return this.top
+  }
+
+  isEmpty() {
+    if( this.top !== 0 ){
+      return true
+    }else{
+      return false
+    }
+  }
+
+  peek() {
+    if( this.top === 0 ){
+      return null
+    }else{
+      return this.element[ this.top ]
+    }
+  }
+
 }
