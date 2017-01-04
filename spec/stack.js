9aecb22e4722cf9e07c4cf25dbf35209ade8e0e7
anasauce <anasauceda1843@gmail.com>
2017-01-03 17:57:50 -0800

    Implement Stack tests and functionality

--- spec/stack.js
+++ spec/stack.js
@@ -5,18 +5,53 @@ import Stack from '../src/stack'
 chai.use(chaiChange)
 
 describe('Stack', () => {
-  'use strict'
 
   it('exists', () => {
-    expect(Stack).to.be.a('function')
+    expect(Stack).to.be.a( 'function' )
   })
 
-  context('push()', () => {
-    it('pushes an element to the top of the stack.', () => {
-      const awesomeStack = new Stack()
+  context('stack simulation', () => {
+    const awesomeStack = new Stack()
 
-      expect(() => awesomeStack.push('foo'))
+    it('beef up the stack\'s commissary by adding a new element to the top', () => {
+      expect(() => awesomeStack.beefUp( 'secret' ))
         .to.alter(() => awesomeStack.length(), { from: 0, to: 1 })
     })
+
+    it('beefing up the stack\'s commissary increases its topIndex', () => {
+      expect(awesomeStack.topIndex).to.equal( 0 )
+    })
+
+    it('amputate the top most element from the stack\'s commissary', () => {
+      expect(() => awesomeStack.amputatePiece())
+        .to.alter(() => awesomeStack.length(), { from: 1, to: 0 })
+    })
+
+    it('amputating a piece from the stack\'s commissary decreases the topIndex', () => {
+      expect(awesomeStack.topIndex).to.equal( null )
+    })
+
+    it('amputating a piece from the stack\'s commissary decreases the size', () => {
+      expect(awesomeStack.size).to.equal( 0 )
+    })
+
+    it('should return an overflow error if amputation is attempted on an empty commissary', () => {
+      expect(awesomeStack.amputatePiece()).to.equal( 'underflow error' )
+    })
+
+    it('glancing at an empty commissary returns null', () => {
+      expect(awesomeStack.glance()).to.equal( null )
+    })
+
+    it('glancing reveals the secret located at the top index of our commissary', () => {
+      awesomeStack.beefUpMany( 'secretOne', 'two', 'three' )
+      expect(awesomeStack.glance()).to.equal( 'three' )
+    })
+
+    it('will return true if the commissary is uninhabited', () => {
+      awesomeStack.amputatePieceMany( 3 )
+      expect(awesomeStack.isUninhabited()).to.equal( true )
+    })
+
   })
 })
--- src/stack.js
+++ src/stack.js
@@ -1,17 +1,49 @@
-'use strict'
-
-let awesomeStack = []
-
 export default class Stack {
   constructor() {
-    this.commissary = []
+    this.commissary = [],
+    this.size = 0,
+    this.topIndex = null
   }
 
-  push( item ) {
-    this.commissary.push( item )
+  beefUp( item ) {
+    this.commissary[this.size] = item
+    this.topIndex = this.size
+    this.size++
+  }
+
+  beefUpMany( ...items ) {
+    items.forEach( item => {
+      this.beefUp( item )
+    })
+  }
+
+  amputatePiece() {
+    if (this.size > 0) {
+      this.size--
+      this.size > 0 ? this.topIndex = this.size - 1 : this.topIndex = null
+      return this.commissary.splice(-1, 1)
+    } else {
+      return 'underflow error'
+    }
+  }
+
+  amputatePieceMany( number ) {
+    let amputatedPieces = []
+    for( let index = 0; index < number; index++ ) {
+      amputatedPieces.push(this.amputatePiece())
+    }
+    return amputatedPieces
+  }
+
+  glance() {
+    return this.size > 0 ? this.commissary[this.topIndex] : null
+  }
+
+  isUninhabited() {
+    return this.size > 0 ? false : true
   }
 
   length() {
-    return this.commissary.length
+    return this.size
   }
 }
