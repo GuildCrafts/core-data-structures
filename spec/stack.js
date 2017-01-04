3d6684954c2204bd042933879213a9c7eb977667
firegoboom <miketheklein@gmail.com>
2017-01-03 16:14:20 -0800

    started on this grand journey with adding in stack functionality

--- spec/stack.js
+++ spec/stack.js
@@ -13,10 +13,24 @@ describe('Stack', () => {
 
   context('push()', () => {
     it('pushes an element to the top of the stack.', () => {
-      const myStack = new Stack()
+      const aStack = new Stack()
 
-      expect(() => myStack.push('foo'))
-        .to.alter(() => myStack.length(), { from: 0, to: 1 })
+      expect(() => aStack.push('foo'))
+        .to.alter(() => aStack.length(), { from: 0, to: 1 })
     })
   })
+
+  context('pop()', () => {
+    it('removes an element from the top of the stack.', () => {
+      const aStack = new Stack( 1 )
+      expect(() => aStack.pop())
+        .to.alter(() => aStack.length(), { from: 1, to: 0 })
+    })
+  })
+  //
+  // stack.peek()      // returns the top element in the stack or null if the stack is empty.
+  //
+  // stack.isEmpty()   // returns true if the stack is empty or false if not.
+  //
+  // stack.length()    // returns the number of elements in the stack.}
 })
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,35 @@
 'use strict'
 
+
 export default class Stack {
-  // your code here
+  constructor() {
+    this.top = 0
+    this.elements = []
+  }
+
+  // this.push = find length of array and make sure the length = +1 after function runs
+  push( thing ) {
+    this.elements[this.top] = thing
+    this.top++
+  }
+
+  // this.pop = find length of array and make sure length = -1 and it was taken off of the beginning
+  pop() {
+    this.elements.shift()
+    this.top--
+  }
+
+  length() {
+    return this.top
+  }
+
+
+  //
+  //
+  // this.peek = return stackArray([0])
+  //
+  // this.isEmpty = stackArray() === null ? true : false
+  //
+  // this.length = return stackArray().size
+
 }
