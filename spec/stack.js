2644998c289ad24fea29e6e21d5e6bb259486478
Serafin <leaf466@gmail.com>
2017-01-10 09:48:18 -0800

    Add testing and functionality for stack data structure

--- spec/stack.js
+++ spec/stack.js
@@ -4,21 +4,86 @@ import Stack from '../src/stack'
 
 chai.use(chaiChange)
 
-describe('Stack', function() {
+describe('Stack', () => {
   'use strict'
 
-  it('exists', function() {
+  it('exists', () => {
     expect(Stack).to.be.a('function')
   })
 
-  context('push()', function() {
-    it('pushes an element to the top of the stack.', function() {
+  context('push()', () => {
+    it('pushes an element to the top of the stack.', () => {
 
-      const stackOfMoney = new Stack
+      const stackOfMoney = new Stack()
 
-      expect(function(){ stackOfMoney.push('foo')})
-        .to.alter( this.stackOfMoney.length, { from: 0, to: 1 })
+      expect(() => { stackOfMoney.push('foo')})
+        .to.alter( () => {
+          return stackOfMoney.lengthVal
+        }, { from: 0, to: 1 })
     })
+  })
 
+  context('pop()', () => {
+    const stackOfMoney = new Stack()
+
+    it('takes off the top element from the stack reducing length.', () => {
+
+      stackOfMoney.push('tacos')
+
+      expect(() => {
+        stackOfMoney.pop()
+      })
+        .to.alter( () => {
+          return stackOfMoney.lengthVal
+        }, { from: 1, to: 0 })
+    }),
+
+    it('returns the top most object\'s content', () => {
+
+      stackOfMoney.push('tacos')
+
+      expect(stackOfMoney.pop()).to.equal( 'tacos' )
+    })
+  })
+
+  context('length()', () => {
+    const stackOfMoney = new Stack()
+
+    it('returns the number of elements in the stack.', () => {
+
+      stackOfMoney.push('tacos')
+      stackOfMoney.push('shishkabobs')
+
+      expect(stackOfMoney.length()).to.equal( 2 )
+    })
+  })
+
+  context('peek()', () => {
+    const stackOfMoney = new Stack()
+
+    it('returns null if the stack is empty.', () => {
+
+      expect(stackOfMoney.peek()).to.equal( null )
+    }),
+
+    it('returns the top element if not null.', () => {
+      stackOfMoney.push('dollar')
+
+      expect(stackOfMoney.peek()).to.equal( 'dollar' )
+    })
+  })
+
+  context('isEmpty()', () => {
+    const stackOfMoney = new Stack()
+
+    it('returns true if the stack is empty.', () => {
+      expect(stackOfMoney.isEmpty()).to.equal( true )
+    }),
+
+    it('returns false if the stack is not empty.', () => {
+      stackOfMoney.push('dollar')
+
+      expect(stackOfMoney.isEmpty()).to.equal( false )
+    })
   })
 })
--- src/stack.js
+++ src/stack.js
@@ -2,11 +2,48 @@
 
 export default class Stack {
   constructor(){
-    this.stack = []
+    this.head = null
+    this.lengthVal = 0
   }
 
   push( item ){
-     this.stack.push(item)
+    this.lengthVal++
+    if( this.head === null) {
+      this.head = {
+        element: item,
+        next: null
+      }
+    } else {
+      let temp = this.head
+      this.head = {
+        element: item,
+        next: temp
+      }
+    }
   }
 
+  pop(){
+    if( this.head !== null){
+      this.lengthVal--
+      const temp = this.head
+      this.head = this.head.next
+      return temp.element
+    }
+  }
+
+  length(){
+    return this.lengthVal
+  }
+
+  peek(){
+    if( this.head !== null){
+      return this.head.element
+    } else {
+      return null
+    }
+  }
+
+  isEmpty(){
+    return this.lengthVal <= 0
+  }
 }
