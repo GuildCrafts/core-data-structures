0c2237f61ade4b5dd8f0af702f6e96e3f3d977ce
John Hallman <jhallman5@Johns-MacBook-Pro.local>
2017-01-10 15:03:06 -0800

    queue and queue test

---
+++ src/queue.js
@@ -0,0 +1,45 @@
+export default class Queue {
+  constructor () {
+    this.queue = [];
+    this.index = 0
+  }
+
+  enqueue(item) {
+    this.queue[this.index++] = item;
+  }
+
+  length(){
+    return this.index
+  }
+
+  dequeue(){
+    return this.queue.shift(0)
+  }
+
+  front(){
+    //ask jrob
+    if(this.index === 0) {
+     return null
+    }
+    else {
+     return this.queue[0]
+    }
+  }
+
+  back() {
+    // if(this.index === 0) {
+    //  return null
+    // }
+    // else {
+    //  return this.queue[this.index - 1]
+    // }
+
+    return this.index > 0 ? this.queue[this.index - 1] : null
+  }
+
+  isEmpty() {
+    if(this.index === 0) {
+     return true
+    }
+  }
+}
