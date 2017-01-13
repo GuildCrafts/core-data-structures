bce969386233d1de0b9f16882e01670b085952e9
Tanner Welsh <tannerwelsh@gmail.com>
2017-01-12 21:52:05 -0500

    Test and implement Stack

--- spec/stack.js
+++ spec/stack.js
@@ -19,4 +19,59 @@ describe('Stack', () => {
         .to.alter(() => myStack.length(), { from: 0, to: 1 })
     })
   })
+
+  context('pop()', () => {
+    it('returns and removes the top element in the stack', () => {
+      const myStack = new Stack()
+
+      myStack.push('bar')
+      myStack.push('foo')
+
+      const foo = myStack.pop()
+      expect(foo).to.eq('foo')
+
+      expect(() => myStack.pop())
+        .to.alter(() => myStack.length(), { from: 1, to: 0 })
+    })
+  })
+
+  context('peek()', () => {
+    it('returns the top element in the stack', () => {
+      const myStack = new Stack()
+
+      myStack.push('foo')
+      expect(myStack.peek()).to.eq('foo')
+    })
+
+    context('with empty stack', () => {
+      it('returns null', () => {
+        const myStack = new Stack()
+
+        expect(myStack.peek()).to.eq(null)
+      })
+    })
+  })
+
+  context('isEmpty()', () => {
+    it('returns true if the stack is empty or false if not', () => {
+      const emptyStack = new Stack()
+      const nonEmptyStack = new Stack()
+
+      nonEmptyStack.push('foo')
+
+      expect(emptyStack.isEmpty()).to.eq(true)
+      expect(nonEmptyStack.isEmpty()).to.eq(false)
+    })
+  })
+
+  context('length()', () => {
+    it('returns the number of elements in the stack', () => {
+      const myStack = new Stack()
+
+      myStack.push('bar')
+      myStack.push('foo')
+
+      expect(myStack.length()).to.eq(2)
+    })
+  })
 })
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,33 @@
 'use strict'
 
 export default class Stack {
-  // your code here
+  constructor() {
+    this._store = []
+  }
+
+  push(element) {
+    return this._store.push(element)
+  }
+
+  pop() {
+    return this._store.pop()
+  }
+
+  peek() {
+    const length = this.length()
+
+    if (length === 0) {
+      return null
+    }
+
+    return this._store[length - 1]
+  }
+
+  isEmpty() {
+    return this.length() === 0
+  }
+
+  length() {
+    return this._store.length
+  }
 }
