280169cd29dcbbcbca0b01998e887373f5794441
FrankieFabuloso <frocha@ucsc.edu>
2017-01-12 16:35:27 -0800

    adds remaining LL methods along with passing unit tests

--- src/linkedList.js
+++ src/linkedList.js
@@ -3,21 +3,28 @@ import Node from './node'
 export default class LinkedList {
   constructor() {
     this.head = null
-    this.size = 0
+    this.listSize = 0
   }
 
   getHeadNode() {
+    if(this.head == null) {
+      throw new Error(`Trying to get head of empty List.`)
+    }
     return this.head
   }
 
   getTailNode() {
+    if(this.head == null) {
+      throw new Error(`Trying to get tail of empty List.`)
+    }
+
     let cursor = this.head
     while( cursor.next ) { cursor = cursor.next }
     return cursor
   }
 
   contains( data ) {
-
+    return this.find(data) != -1
   }
 
   find( data ) {
@@ -34,29 +41,33 @@ export default class LinkedList {
     //head is first node inserted
     const newNode = new Node( data )
     let cursor = this.head
-    if(this.size === 0) {
+    if(this.listSize === 0) {
       this.head = newNode
     } else {
       //keep traversiing untill cursor.next is null
       while( cursor.next ) { cursor = cursor.next }
       cursor.next = newNode
     }
-    this.size++
+    this.listSize++
   }
   // adds to head
   insertFirst( data ) {
     const newNode = new Node( data )
-    if(this.size === 0 ) {
+    if(this.listSize === 0 ) {
       this.head = newNode
     } else {
       newNode.next = this.head
       this.head = newNode
     }
-    this.size++
+    this.listSize++
   }
 
   insertBefore( frontData, newData ) {
     const frontNode = this.find( frontData )
+    if( frontNode == -1 ) {
+      throw new Error(`Target: ${newData}, does not exist in List `)
+    }
+
     const newNode = new Node( newData )  //has our new data
     let beforeFN = this.head
     while( beforeFN.next != null && beforeFN.next != frontNode ) {
@@ -67,14 +78,51 @@ export default class LinkedList {
     } else {
       newNode.next = frontNode
       beforeFN = newNode
+      this.listSize++
     }
   }
 
   insertAfter( behindData, newData ) {
     const behindNode = this.find( behindData )
     const newNode = new Node( newData )
+    if( behindNode == -1 ) {
+      throw new Error(`Target: ${newData}, does not exist in List `)
+    }
+
     newNode.next = behindNode.next
     behindNode.next = newNode
+    this.listSize++
+  }
+
+  remove() {
+    if(this.listSize < 1) { throw new Error('List is empty. Nothing to remove.')}
+
+    let cursor = this.head
+    while(cursor.next && cursor.next != this.getTailNode()) { cursor = cursor.next }
+    cursor.next = null
+    this.listSize--
+  }
+
+  removeFirst() {
+    if(this.listSize < 1) { throw new Error('List is empty. Nothing to remove.')}
+
+    let newHead = this.head.next
+    this.head.next = null
+    this.head = newHead
+    this.listSize--
+  }
+
+  isEmpty() {
+    return this.listSize === 0
+  }
+
+  size() {
+    return this.listSize
+  }
+
+  clear() {
+    this.head = null
+    this.listSize = 0
   }
 
   toString() {
--- tests/linkedListTest.js
+++ tests/linkedListTest.js
@@ -11,11 +11,43 @@ describe('LinkedList', () => {
     expect(LinkedList).to.be.a('function')
   })
 
+  context('getHeadNode()', () => {
+    it('Returns head node of list', () => {
+      const myLinkedList = new LinkedList()
+      expect(() => myLinkedList.getHeadNode()).to.throw(Error)
+      myLinkedList.insert('fart')
+      myLinkedList.insert('poop')
+      expect(myLinkedList.getHeadNode().data).to.equal('fart')
+      myLinkedList.removeFirst()
+      expect(myLinkedList.getHeadNode().data).to.equal('poop')
+    })
+  })
+
+  context('contains()', () => {
+    it('Returns true if target data is in list', () => {
+      const myLinkedList = new LinkedList()
+      myLinkedList.insert('fart')
+      myLinkedList.insert('poop')
+      expect(myLinkedList.contains('poop')).to.equal(true)
+      expect(myLinkedList.contains('lemonade')).to.equal(false)
+    })
+  })
+
+  context('contains()', () => {
+    it('Returns true if target data is in list', () => {
+      const myLinkedList = new LinkedList()
+      myLinkedList.insert('fart')
+      myLinkedList.insert('poop')
+      expect(myLinkedList.contains('poop')).to.equal(true)
+      expect(myLinkedList.contains('lemonade')).to.equal(false)
+    })
+  })
+
   context('insert()', () => {
     it('inserts a node (with the provided data) to the tail of the list', () => {
       const myLinkedList = new LinkedList()
       expect(() => myLinkedList.insert('box'))
-        .to.alter(() => myLinkedList.size, { from: 0, to: 1})
+        .to.alter(() => myLinkedList.listSize, { from: 0, to: 1})
       myLinkedList.insert('farts')
       myLinkedList.insert('poop')
       myLinkedList.toString()
@@ -23,7 +55,27 @@ describe('LinkedList', () => {
       expect(myLinkedList.getTailNode().data).to.equal('poop')
       myLinkedList.insert('last thing')
       expect(myLinkedList.getTailNode().data).to.equal('last thing')
+      expect(myLinkedList.listSize).to.equal(4)
       myLinkedList.toString()
+    })
+  })
+
+  context('insertFirst()', () => {
+    it('inserts a node (with the provided data) to the head of the list', () => {
+      const myLinkedList = new LinkedList()
+      expect(() => myLinkedList.insertFirst('box'))
+      .to.alter(() => myLinkedList.listSize, { from: 0, to: 1})
+      myLinkedList.insertFirst('farts')
+      myLinkedList.insertFirst('poop')
+      myLinkedList.toString()
+      expect(myLinkedList.getHeadNode().data).to.equal('poop')
+      expect(myLinkedList.getTailNode().data).to.equal('box')
+      myLinkedList.insertFirst('first thing')
+      expect(myLinkedList.getHeadNode().data).to.equal('first thing')
+      myLinkedList.insert('last thing')
+      expect(myLinkedList.getTailNode().data).to.equal('last thing')
+      myLinkedList.toString()
+      expect(myLinkedList.listSize).to.equal(5)
     })
   })
 
@@ -35,11 +87,9 @@ describe('LinkedList', () => {
       myLinkedList.insert('rain')
       myLinkedList.toString()
       expect(myLinkedList.find('sunshine').data).to.equal('sunshine')
-      console.log(myLinkedList.find())
       expect(myLinkedList.find('pain')).to.equal(-1)
     })
   })
-
 
   context('insertBefore()', () => {
     it('Inserts a node with data before the first node containing target data', () => {
@@ -49,6 +99,8 @@ describe('LinkedList', () => {
       myLinkedList.insertBefore('bananas', 'apples')
       myLinkedList.toString()
       expect(myLinkedList.find('apples').next.data).to.equal('bananas')
+      expect(() => myLinkedList.insertBefore('poop', 'shoe')).to.throw(Error)
+      expect(myLinkedList.listSize).to.equal(2)
     })
   })
 
@@ -61,24 +113,70 @@ describe('LinkedList', () => {
       expect(myLinkedList.find('farts').next.data).to.equal('balls')
       myLinkedList.insert('lemons')
       expect(myLinkedList.find('balls').next.data).to.equal('lemons')
+      expect(() => myLinkedList.insertAfter('poop', 'shoe')).to.throw(Error)
+      expect(myLinkedList.listSize).to.equal(3)
     })
   })
 
-  context('insertFirst()', () => {
-    it('inserts a node (with the provided data) to the head of the list', () => {
+  context('remove()', () => {
+    it('Removes tail from list.', () => {
       const myLinkedList = new LinkedList()
-      expect(() => myLinkedList.insertFirst('box'))
-        .to.alter(() => myLinkedList.size, { from: 0, to: 1})
-      myLinkedList.insertFirst('farts')
-      myLinkedList.insertFirst('poop')
-      myLinkedList.toString()
-      expect(myLinkedList.getHeadNode().data).to.equal('poop')
-      expect(myLinkedList.getTailNode().data).to.equal('box')
-      myLinkedList.insertFirst('first thing')
-      expect(myLinkedList.getHeadNode().data).to.equal('first thing')
-      myLinkedList.insert('last thing')
-      expect(myLinkedList.getTailNode().data).to.equal('last thing')
-      myLinkedList.toString()
+      expect(() => myLinkedList.remove()).to.throw(Error)
+      myLinkedList.insert('farts')
+      myLinkedList.insert('cheese')
+      expect(myLinkedList.getTailNode().data).to.equal('cheese')
+      expect(myLinkedList.listSize).to.equal(2)
+      myLinkedList.remove()
+      expect(myLinkedList.getTailNode().data).to.equal('farts')
+      expect(myLinkedList.listSize).to.equal(1)
     })
   })
+
+  // linkedList.removeFirst()
+  context('removeFirst()', () => {
+    it('Removes head from list.', () => {
+      const myLinkedList = new LinkedList()
+      expect(() => myLinkedList.removeFirst()).to.throw(Error)
+      myLinkedList.insert('farts')
+      myLinkedList.insert('cheese')
+      expect(myLinkedList.getTailNode().data).to.equal('cheese')
+      expect(myLinkedList.listSize).to.equal(2)
+      myLinkedList.removeFirst()
+      expect(myLinkedList.getTailNode().data).to.equal('cheese')
+      expect(myLinkedList.listSize).to.equal(1)
+    })
+  })
+
+  context('isEmpty()', () => {
+    it('Returns true is List is empty, false otherwise.', () => {
+      const myLinkedList = new LinkedList()
+      expect(myLinkedList.isEmpty()).to.equal(true)
+      myLinkedList.insert('fart')
+      expect(myLinkedList.isEmpty()).to.equal(false)
+      myLinkedList.remove()
+      expect(myLinkedList.isEmpty()).to.equal(true)
+    })
+  })
+
+  context('size()', () => {
+    it('Returns listSize of the List', () => {
+      const myLinkedList = new LinkedList()
+      expect(myLinkedList.size()).to.equal(0)
+      myLinkedList.insert('fart')
+      expect(myLinkedList.size()).to.equal(1)
+      myLinkedList.remove()
+      expect(myLinkedList.size()).to.equal(0)
+    })
+  })
+
+  context('clear()', () => {
+    it('Clears the list of all nodes/data', () => {
+      const myLinkedList = new LinkedList()
+      myLinkedList.insert('fart')
+      myLinkedList.insert('poop')
+      myLinkedList.clear()
+      expect(myLinkedList.size()).to.equal(0)
+    })
+  })
+
 })
