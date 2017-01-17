817714b20b0ac36bdab9975000c387f6501b0f48
Mark Suzuki <marksuzuki@gmail.com>
2017-01-17 15:49:32 -0800

    working on stack

--- src/stack.js
+++ src/stack.js
@@ -2,4 +2,38 @@
 
 export default class Stack {
   // your code here
+  // always add and remove to the back of the array
+  // let stack = [];
+  // to push data
+  // stack.push(a)
+  // stack.push(b)
+  constructor(size, arr){
+    this.size = 0
+    this.arr = []
+  }
+
+    add(element) {
+      this.arr.push(element)
+    }
+
+    remove(element){
+      this.arr.pop(element)
+    }
+
+    peek(arr) {
+      if(this.arr.length > 0){
+        return this.arr[length-1];
+      } else {
+        return null;
+      }
+    }
+
+  isEmpty(arr){
+    return this.arr.length > 0
+  }
+
+  size(arr){
+    return this.arr.length
+  }
+
 }
