f179393538d9228ed1a6a489571cdee0e400293e
Breyana Scales <breyana@Breyanas-MacBook-Pro.local>
2017-01-18 14:15:19 -0800

    refactored Stack

--- src/stack.js
+++ src/stack.js
@@ -3,46 +3,44 @@
 export default class Stack {
   constructor(initialValues=[]) {
     this.elements = initialValues
-    this.top = initialValues.length
   }
 
-    push(value) {
-      this.elements[this.top] = value
-      this.top++
-    }
+  push(value) {
+    this.elements[this.elements.length] = value
 
-    pop() {
-      if(this.top === 0){
-        return null
-      }
-      let result = this.elements.slice(-1)
-      this.elements.splice(-1)
-      this.top--
-      return result
+  }
 
+  pop() {
+    if(this.elements.length === 0){
+      return null
     }
+    let result = this.elements.slice(-1)
+    this.elements.splice(-1)
+    return result
 
-    peek() {
-      if (this.top === 0){
-        return null
-      }
-      return this.elements[this.top -1]
-    }
+  }
 
-    isEmpty() {
-      if (this.top === 0 ){
-        return true
-      } else {
-        return false
-      }
+  peek() {
+    if (this.elements.length === 0){
+      return null
     }
+    return this.elements[this.elements.length -1]
+  }
 
-    length() {
-      let count = 0
-      for (var key in this.elements) {
-          count++
-      }
-      return count
+  isEmpty() {
+    if (this.elements.length === 0 ){
+      return true
+    } else {
+      return false
     }
+  }
+
+  length() {
+    let count = 0
+    for (var key in this.elements) {
+        count++
+    }
+    return count
+  }
 
 }
