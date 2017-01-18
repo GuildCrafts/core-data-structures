0fe25a2d79816a16cd7dffbd4715baf0e7cd5914
lilvina <davinaleo2007@yahoo.com>
2017-01-18 10:51:07 -0800

    one test is passing

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,33 @@
-'use strict'
+// class Node {
+// 	constructor(data, next, prev) {
+// 		this.data = data
+// 		this.next = next
+// 		this.prev = prev
+// 	}
+// }
 
-export default class Stack {
-  // your code here
+class Stack {
+	constructor() {
+		this.arr = []
+		this.top = 0
+	}
+	push(element) {
+		this.arr[this.top++] = element
+	}
+	length() {
+		
+	}
 }
+
+// const stack = new Stack()
+// stack.push("foo")
+// console.log(stack)
+
+export default Stack
+
+
+// 'use strict'
+// export default class Stack {
+//   // your code here
+
+// }
