fdb35b5b13f114a2f2cf541fa5e3018a0419a099
lilvina <davinaleo2007@yahoo.com>
2017-01-18 18:00:37 -0800

    test for pop works

--- src/stack.js
+++ src/stack.js
@@ -5,4 +5,38 @@ export default class Stack {
   push(element) {
     return this.arr.push(element)
   }
+
+  pop() {
+  	return this.arr.pop()
+  }
+
+  length() {
+  	return this.arr.length()
+  }
 }
+
+// class Stack {
+//   constructor() {
+//   	this.arr = new Array()
+//   }
+
+//   push(element) {
+//     return this.arr.push(element)
+//   }
+
+//   pop() {
+//   	return this.arr.pop()
+//   }
+
+//   length() {
+//   	return this.arr.length()
+//   }
+// }
+
+// const stack = new Stack()
+// stack.push(10)
+// stack.push(11)
+// stack.push(12)
+// console.log(stack.length())
+
+// export default Stack
