565e002eddb3583f6a35996a84edde3ceec0b148
lisabronwyn <e.bronwyn@gmail.com>
2017-01-20 16:29:50 -0800

    trying to push branch with binarytree work with incomplete tests

---
+++ spec/binarytree.js
@@ -0,0 +1,14 @@
+import chai, { expect } from 'chai'
+import chaiChange from 'chai-change'
+import PriorityQueue from '../src/binarytree'
+
+chai.use(chaiChange)
+
+describe('binarySearchTree', () => {
+  'use strict'
+
+  it('exists', () => {
+    expect(binarySearchTree).to.be.a('function')
+
+  })
+})
