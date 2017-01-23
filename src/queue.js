0b825def8de4e4c246b550672b8a15bf444ffbc9
lilvina <davinaleo2007@yahoo.com>
2017-01-23 15:34:09 -0800

    queue: enqueue, dequeue, front and back working

---
+++ src/queue.js
@@ -0,0 +1,40 @@
+'use strict'
+
+export default class Queue {
+  constructor() {
+    this.storage = []
+  }
+
+  enqueue(element) {
+    return this.storage.push(element)
+  }
+
+  length() {
+    return this.storage.length
+  }
+
+  dequeue() {
+    if(this.storage.length === 0) {
+      return null
+    } else {
+      return this.storage.pop(this.storage.length-1)
+    }
+  }
+
+  front() {
+    if(this.storage.length === 0) {
+      return null
+    } else {
+      return this.storage[this.storage.length-1]
+    }
+  }
+
+  back() {
+    if(this.storage.length === 0) { 
+      return null
+    } else {
+      return this.storage[0]
+    }
+  }
+
+}
