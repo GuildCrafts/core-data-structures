7a3887443f8984618faccb62ee5ec2e7d73243b3
Ethan Stark <ethanstark@gmail.com>
2017-01-24 11:10:07 -0800

    Stack: Implementation and tests

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,31 @@
 'use strict'
 
 export default class Stack {
-  // your code here
+  constructor(){
+    this._storage = []
+    this._topIndex = -1
+  }
+
+  length() {
+    return this._topIndex + 1
+  }
+
+  push(data) {
+    this._storage[ ++this._topIndex ] = data
+  }
+
+  peek() {
+    return this._topIndex === -1 ? null : this._storage[ this._topIndex ]
+  }
+
+  isEmpty() {
+    return this._topIndex === -1
+  }
+
+  pop() {
+    if ( this._topIndex === -1 ) { return null }
+    let result = this._storage[ this._topIndex ]
+    delete this._storage[ this._topIndex-- ]
+    return result
+  }
 }
