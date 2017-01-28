20488c743ed5005e451f5d6d2976887a3aee0c2d
yaseenagag <yaseen.uc@gmail.com>
2017-01-27 16:22:24 -0800

    implement linkedList and tests WIP

--- spec/linked_test.js
+++ spec/linked_test.js
@@ -1,25 +1,102 @@
 import chai, { expect } from 'chai'
 import chaiChange from 'chai-change'
-import linkedList from '../src/linkedList'
+import { linkedList, Node } from '../src/linkedList'
+
 
 chai.use(chaiChange)
 
-describe.only('linkedList', () => {
+describe('linkedList', () => {
 
   it('exists', () => {
     expect(linkedList).to.be.a('function')
   })
 
-  context('getHeadNode()', () => {
-    it('Returns the first node in the llist', () => {
-      const llist = new linkedList()
-      llist.add(2)
-      console.log('llist', llist)
-      expect( llist.getHeadNode() ).to.eql( {data : 2, next: null})
+  context( 'getNext()', () => {
+    it ( 'exist', () => {
+      const node = new Node('hi')
+      expect ( node.getData() ).to.equal( 'hi' )
     })
-    it('Returns the first node in the llist', () => {
+  })
+
+  context('getHeadNode()', () => {
+
+    it('Returns the first node in the list 1', () => {
       const llist = new linkedList()
-        expect( llist.getHeadNode()).to.eql( null )
+      console.log('llist', llist)
+      expect( llist.getHeadNode()).to.eql( null )
+    })
+    it('Returns the first node in the list 2', () => {
+      const llist = new linkedList()
+      llist.insert('a')
+      console.log('llist', llist)
+      expect( llist.getHeadNode() ).to.eql( { data : 'a', nextNode : null} )
+    })
+    it('Returns the first node in the list 3', () => {
+      const llist = new linkedList()
+      llist.insert('a')
+      llist.insert('b')
+      console.log('llist', llist)
+      expect( llist.getHeadNode() )
+        .to.eql( { data : 'a', nextNode : {data : 'b', nextNode : null}} )
+    })
+
+    it('Returns the first node in the list 4', () => {
+      const llist = new linkedList()
+      llist.insert('a')
+      llist.insert('b')
+      llist.insert('c')
+      console.log('llist', llist)
+      expect( llist.getHeadNode() )
+        .to.eql({ data : 'a', nextNode : {data : 'b', nextNode :{data : 'c', nextNode : null}}} )
+    })
+  })
+
+  context('insertFirst()', () => {
+
+    it('Inserts a node (with the provided data) to the head of the list', () => {
+      const llist = new linkedList()
+      llist.insertFirst('a')
+      llist.insertFirst('b')
+      llist.insertFirst('c')
+      console.log('llist', llist)
+      expect( llist.getHeadNode() )
+        .to.eql({ data : 'c', nextNode : {data : 'b', nextNode :{data : 'a', nextNode : null}}} )
+    })
+  })
+
+  context('insertBefore()', () => {
+
+    it('Inserts a node (with data "ardvark") before the first node containing "cat"', () => {
+      const llist = new linkedList()
+      llist.insert('antlion')
+      llist.insert('beaver')
+      llist.insert('cat')
+      llist.insert('gorilla')
+      expect( llist.getHeadNode() )
+        .to.eql({ data : 'antlion', nextNode : {data : 'beaver', nextNode :{data : 'cat', nextNode : {data : 'gorilla', nextNode : null}}}} )
+
+      llist.insertBefore('ardvark', 'cat')
+
+      expect( llist.getHeadNode() )
+        .to.eql({ data : 'antlion', nextNode : {data : 'beaver', nextNode :{data : 'ardvark', nextNode : {data : 'cat', nextNode : {data : 'gorilla', nextNode : null}}}}} )
+    })
+  })
+
+  context('insertAfter()', () => {
+
+    it.only('Inserts a node (with data "ardvark") after the first node containing "cat"', () => {
+      const llist = new linkedList()
+      llist.insert('antlion')
+      llist.insert('beaver')
+      llist.insert('cat')
+      llist.insert('gorilla')
+      expect( llist.getHeadNode() )
+        .to.eql({ data : 'antlion', nextNode : {data : 'beaver', nextNode :{data : 'cat', nextNode : {data : 'gorilla', nextNode : null}}}} )
+
+      llist.insertAfter('ardvark', 'cat')
+
+      expect( llist.getHeadNode() )
+        .to.eql({ data : 'antlion', nextNode : {data : 'beaver', nextNode :{data : 'cat', nextNode : {data : 'ardvark', nextNode : {data : 'gorilla', nextNode : null}}}}} )
     })
   })
 
--- src/linkedList.js
+++ src/linkedList.js
@@ -1,25 +1,25 @@
 'use strict'
 
 class Node {
-    constructor( data, next = null ) {
-      this.data = data
-      this.next = next
-    }
+  constructor( data, node = null ) {
+    this.data = data
+    this.nextNode = node
+  }
 
   getData() {
     return this.data
   }
 
   getNext() {
-    return this.next
+    return this.nextNode
   }
 
-  setNext() {
-    this.next = next
+  setNext( node ) {
+    this.nextNode = node
   }
 }
 
-export default class linkedList {
+ class linkedList {
 //creates linkedList
   constructor() {
     this.length = 0
@@ -27,28 +27,130 @@ export default class linkedList {
     this.tail = null
   }
 
+  insert(data) {
+    const node = new Node(data, null)
 
-
-  add(data) {
-    const node = new Node(data)
-
-    if( this.head === null ) {
+    if( this.isEmpty() ) {
       this.head = this.tail = node
-      // this.tail = node
     } else {
       this.tail.setNext(node)
-      this.tail = node
+      this.tail = this.tail.getNext()
+      // this.tail = node
     }
     this.length++
   }
 
+  insertFirst(data) {
+    const node = new Node(data, null)
 
-  getHeadNode() {
-    if( this.tail === undefined ) {
+    if( this.isEmpty() ) {
+      this.head = this.tail = node
+    } else {
+      node.setNext(this.head)
+      this.head = node
+    }
+    this.length++
+  }
+
+  insertBefore(data, positionData){
+    const node = new Node(data, null)
+
+    if( this.isEmpty() ) {
       return null
     } else {
-      return this.tail
+      let nodeRight = this.find(positionData)
+      if(nodeRight !== -1){ // nodeRight does indeed exist
+        if(nodeRight === this.getHeadNode()){
+          this.head = node
+        } else {
+          let nodeLeft = this.findPrev(positionData)
+          nodeLeft.setNext(node)
+        }
+        node.setNext(nodeRight)
+      } else {
+        return null
+      }
     }
+    this.length++
+  }
+
+  insertAfter(data, positionData){
+    if( this.isEmpty() ) return null
+
+    const node = new Node(data, null)
+    let currentNode = this.head
+
+    while (currentNode.nextNode) {
+      if (currentNode.data === positionData) {
+        node.nextNode = currentNode.nextNode 
+        currentNode.nextNode = node
+        this.length++
+        console.log('currentNode =====>', currentNode)
+        break
+      }
+
+      currentNode = currentNode.nextNode
+    }
+
+
+//     if( this.isEmpty() ) {
+//       return null
+//     }
+//     let nodeLeft = this.find(positionData)
+// console.log('nodeLeft', nodeLeft)
+//     if(nodeLeft !== -1){ // nodeLeft does indeed exist
+//       node.next = this.nodeLeft = node
+//       }
+//   }
+
+}
+
+
+
+
+
+
+
+  getHeadNode() {
+    if( this.head === undefined ) {
+      return null
+    } else {
+      return this.head
+    }
+  }
+
+  find(data){
+    if ( this.isEmpty() ){
+      return -1
+    }
+    let iterator = this.head
+    while(iterator){
+      if(data === iterator.data) return iterator
+      iterator = iterator.getNext()
+    }
+    return -1
+  }
+
+  // Helper Function
+  // TODO: Make private
+  findPrev(data){
+    if ( this.isEmpty() ){
+      return -1
+    }
+    let iterator = this.head
+    while(iterator.getNext()){
+      if(data === iterator.getNext().data) return iterator
+      iterator = iterator.getNext()
+    }
+    return -1
+  }
+
+  // length() {
+  //   return this.length
+  // }
+  //
+  isEmpty() {
+    return this.length == 0
   }
 
   // getTailNode(){
@@ -59,4 +161,9 @@ export default class linkedList {
   //   }
   // }
 
+}
+
+export {
+  Node,
+  linkedList
 }
