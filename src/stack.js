e759c348f964920314c5a08b43499e8ed72da6fd
Helen Yau <helenyau13@gmail.com>
2017-02-13 15:56:16 -0800

    make first  test pass for stack

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,25 @@
 'use strict'
 
+class Node {
+  constructor(data, next) {
+    this.data = data,
+    this.next = next
+  }
+}
+
+
 export default class Stack {
-  // your code here
+  constructor() {
+    this.size = 0;
+    this.storage = null;
+  }
+
+  push(data) {
+    this.storage = new Node(data, this.storage)
+    this.size++
+  }
+
+  length() {
+    return this.size
+  }
 }
