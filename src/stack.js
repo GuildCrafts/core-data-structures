6d305d749680ec46f72df619764068ca4a44de40
Diana Vashti <diana.vashti@gmail.com>
2017-02-13 14:33:48 -0800

    Adds push and pop methods to stack

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,29 @@
 'use strict'
 
 export default class Stack {
-  // your code here
+  const stack = new Stack()
+
+//stack.push("foo") // push an element (the string "foo") to the top of the stack.
+  const push = function(arr, x){
+    if (typeof x === String){
+      return x.split()
+    }
+    return arr.concat(x)
+  }
+
+//stack.pop()       // returns and removes the top element in the stack or null if the stack is empty.
+  const pop = function(arr){
+    var poppedArr = arr.splice(arr.length-1, 1)
+    if (arr.length === 0){
+      return null
+    } return poppedArr
+  }
+
+
+
 }
+
+//const stack = new Stack()
+//stack.peek()      // returns the top element in the stack or null if the stack is empty.
+//stack.isEmpty()   // returns true if the stack is empty or false if not.
+//stack.length()    // returns the number of elements in the stack.
