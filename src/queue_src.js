8863d1ce8961653fba673551e8927895bf2f505c
Helen Yau <helenyau13@gmail.com>
2017-02-14 15:19:50 -0800

    work more on queue

--- src/queue_src.js
+++ src/queue_src.js
@@ -3,18 +3,38 @@ export default class Queue {
     this.dataStore = []
     this.oldIndex = 1
     this.newIndex = 1
+    this.size = 0
   }
+
+  push(element) {
+    this.storage[this.size++] = element
+  }
+
 
   enqueue(element) {
     this.newIndex++
     return this.dataStore[this.newIndex] = element
   }
 
-  dequeue() {
-    return this.dataStore[this.newIndex--] 
+  dequeue(element) {
+    this.newIndex--
+    return this.dataStore[this.newIndex] = element
+  }
+
+  front() {
+    return this.dataStore[0] || null
+  }
+
+  empty() {
+    if(this.dataStore.length == 0) {
+      return true
+    } else {
+      return false
+    }
   }
 
   length() {
   return this.newIndex - this.oldIndex
   }
+
 }
