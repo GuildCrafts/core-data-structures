e0a304395e952b6522df7829cfd58a94dfeed85a
Helen Yau <helenyau13@gmail.com>
2017-02-14 13:05:45 -0800

    create working code for enqueue

--- src/queue_src.js
+++ src/queue_src.js
@@ -1,3 +1,20 @@
 export default class Queue {
+  constructor() {
+    this.dataStore = []
+    this.oldIndex = 1
+    this.newIndex = 1
+  }
 
+  enqueue(element) {
+    this.newIndex++
+    return this.dataStore[this.newIndex] = element
+  }
+
+  dequeue() {
+    return this.dataStore.shift()
+  }
+
+  length() {
+  return this.newIndex - this.oldIndex
+  }
 }
