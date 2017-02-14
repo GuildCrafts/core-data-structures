dd1b17f85db6584a85d2e4706c4ab0de401331c8
Diana Vashti <diana.vashti@gmail.com>
2017-02-13 16:49:56 -0800

    finishes stack with tests

--- src/stack.js
+++ src/stack.js
@@ -1,29 +1,43 @@
-'use strict'
-
 export default class Stack {
-  const stack = new Stack()
+  constructor() {
+    this.size = 0
+    this.elements = []
+  }
+
+//stack.length()    // returns the number of elements in the stack.
+  length() {
+    return this.size
+  }
+
+//stack.isEmpty()   // returns true if the stack is empty or false if not.
+  isEmpty() {
+    return this.size === 0
+  }
 
 //stack.push("foo") // push an element (the string "foo") to the top of the stack.
-  const push = function(arr, x){
-    if (typeof x === String){
-      return x.split()
+  push(elementToAdd) {
+    this.elementToAdd = elementToAdd
+    this.size += 1
+    if (typeof elementToAdd === String){
+      x.split()
     }
-    return arr.concat(x)
+    return this.elements = this.elements.concat(elementToAdd)
   }
+
 
 //stack.pop()       // returns and removes the top element in the stack or null if the stack is empty.
-  const pop = function(arr){
-    var poppedArr = arr.splice(arr.length-1, 1)
-    if (arr.length === 0){
+  pop() {
+    if (this.size === 0){
       return null
-    } return poppedArr
+    } this.size -= 1
+      return this.elements.splice(this.elements.length - 1, 1)
   }
 
-
+//stack.peek()      // returns the top element in the stack or null if the stack is empty.
+  peek() {
+    if (this.elements.length === 0){
+      return null
+    } return this.elements[this.elements.length - 1]
+  }
 
 }
-
-//const stack = new Stack()
-//stack.peek()      // returns the top element in the stack or null if the stack is empty.
-//stack.isEmpty()   // returns true if the stack is empty or false if not.
-//stack.length()    // returns the number of elements in the stack.
