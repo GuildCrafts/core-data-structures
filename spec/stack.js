63e348a17e0531757c0c13e4c098d780f9b4b6b5
mikeadossi <mikeadossi@gmail.com>
2017-02-15 11:22:54 -0800

    write Stack.pop and Stack.peek code

--- spec/stack.js
+++ spec/stack.js
@@ -19,4 +19,22 @@ describe('Stack', () => {
         .to.alter(() => myStack.top, { from: 0, to: 1 })
     })
   })
+
+  context('pop()', () => {
+    it('pops an element from the top of the stack.', () => {
+      const myStack = new Stack()
+      myStack.push('foo')
+      expect(() => myStack.pop())
+        .to.alter(() => myStack.top, { from: 1, to: 0 })
+    })
+  })
+
+  context('peek()', () => {
+    it('returns the top element of the stack (without deleting it).', () => {
+      const myStack = new Stack()
+
+      expect(myStack.peek())
+        .to.eql(undefined)
+    })
+  })
 })
--- src/stack.js
+++ src/stack.js
@@ -14,8 +14,6 @@ export default class Stack {
   peek() {
     return this.dataStore[this.top-1]
   }
-
-
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
