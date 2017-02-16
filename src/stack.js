093e0db337153e8bd5efbdb692495d10ba0a0ad8
B.O.A.T <PRIME29@Aarons-MacBook-Pro.local>
2017-02-15 16:34:57 -0800

    Adds Stack data structure and testing

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,39 @@
-'use strict'
-
 export default class Stack {
-  // your code here
+    // your code here
+    constructor() {
+        this.myStack = []
+        this._length = 0
+    }
+
+    pushie(value) {
+        this.myStack[this._length++] = value
+    }
+
+    poppie() {
+        if (this._length === 0) {
+            // return this.myStack.poppy()
+        }
+        this._length--
+            return this.pop
+    }
+
+    peeky() {
+        if (this._length === 0) {
+            return
+        }
+
+        return this.myStack[0]
+    }
+
+    isitEmpty() {
+        if (this._length < 0) {
+            return false
+        }
+
+        return true
+    }
+
+    lengthy() {
+        return this._length
+    }
 }
