50b402de5611da161f9c5024b1bc83d00a0d4675
Debrena McEwen <debrenamcewen@gmail.com>
2017-02-16 10:18:54 -0800

    add queue functions and tests

--- src/queue.js
+++ src/queue.js
@@ -1,38 +1,51 @@
 export default class Queue {
   constructor() {
-    this.dataStore = [];
-    this.enqueue = enqueue;
-    this.dequeue = dequeue;
-    this.front = front;
-    this.back = back;
-    this.toString = toString;
-    this.empty
+    this.arr = [];
+    this.total = 0;
 }
-
-//
   enqueue(element) {
-
+    this.arr[this.total++] = element
   }
 
   dequeue() {
+    if(this.total == null) {
+      return null
+    }
+   while( this.arr.shift() ) {
+      this.arr[--this.total]
+    }
+   return this.arr
+  }
 
+  length() {
+    return this.total
   }
 
   front() {
-
+    if(this.total = 1 && this.total == null) {
+      return null
+    }else{
+      this.arr.shift()
+      return this.arr
+    }
   }
 
   back() {
+    if(this.total-1 == null) {
+      return null
+    }else{
+      this.arr.pop()
+      return this.arr
+    }
   }
 
-  toString() {
-  }
 
-  push() {
-
-  }
-  peek() {
-
+  isEmpty() {
+    if(this.total > 0) {
+    return true
+  } else{
+    return false
+    }
   }
 
 }
