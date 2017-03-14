84225f564da5db4f8982607e947f0fd65a394efa
Rai Lee <thisisrailee@gmail.com>
2017-03-13 17:40:53 -0700

    Implemented stack push and pop functions

--- spec/stack.js
+++ spec/stack.js
@@ -14,9 +14,29 @@ describe('Stack', () => {
   context('push()', () => {
     it('pushes an element to the top of the stack.', () => {
       const myStack = new Stack()
-
       expect(() => myStack.push('foo'))
         .to.alter(() => myStack.length(), { from: 0, to: 1 })
+        console.log(myStack)
     })
   })
+
+  context('pop()', () => {
+    it('returns and removes the top element in the stack or null if the stack is empty.', () => {
+      const myStack = new Stack()
+      myStack.push('One')
+      console.log(myStack)
+      console.log(myStack.length)
+      expect(() => myStack.pop()).to.alter(() => myStack.length(), { from: 1, to: 0 })
+      console.log(myStack)
+      console.log(myStack.length)
+    })
+
+    it('returns null if the stack is empty', () => {
+      const myStack = new Stack()
+      console.log(myStack)
+      expect( myStack.pop()).to.be.null
+    })
+
+  })
+
 })
