2e8fe9aba88d0a2224e4e34517f3ec359977c6ed
AdamB37 <adam.beshir@gmail.com>
2017-03-28 17:09:10 -0700

    refactor hash table for dynamic resizing instead of linked list for collision handling

--- spec/hash_table.js
+++ spec/hash_table.js
@@ -23,18 +23,18 @@ describe('HashTable', () => {
   it('get(key): gets the value associated with the input key', () => {
     ht.put('asuh','dood')
     ht.put('hoi','mate')
-    ht.put('cigar','waffle')
-    ht.put('smoke','pancake')
+    ht.put('shigar','waffle')
+    ht.put('shmoke','pancake')
     ht.put('pipe', 'bagel')
-    expect(ht.get('cigar')).to.eql('waffle')
+    expect(ht.get('shigar')).to.eql('waffle')
     expect(ht.get('asuh')).to.eql('dood')
   })
 
   it('contains(key): returns true if input key exists in has table', () => {
     ht.put('asuh','dood')
     ht.put('hoi','mate')
-    ht.put('cigar','waffle')
-    ht.put('smoke','pancake')
+    ht.put('shigar','waffle')
+    ht.put('shmoke','pancake')
     ht.put('pipe', 'bagel')
     expect(ht.contains('asuh')).to.equal(true)
     expect(ht.contains('cigarette')).to.equal(false)
@@ -43,8 +43,8 @@ describe('HashTable', () => {
   it('iterate(callback): applies the callback on each key value pair', () => {
     ht.put('asuh','dood')
     ht.put('hoi','mate')
-    ht.put('cigar','waffle')
-    ht.put('smoke','pancake')
+    ht.put('shigar','waffle')
+    ht.put('shmoke','pancake')
     ht.put('pipe', 'bagel')
     let testArray = []
     ht.iterate( (key,value) => {
@@ -60,20 +60,20 @@ describe('HashTable', () => {
   it('remove(key): removes node associated with input key from hash table', () => {
     ht.put('asuh','dood')
     ht.put('hoi','mate')
-    ht.put('cigar','waffle')
-    ht.put('smoke','pancake')
+    ht.put('shigar','waffle')
+    ht.put('shmoke','pancake')
     ht.put('pipe', 'bagel')
     expect(() => ht.remove('asuh'))
     .to.alter(() => ht.size(), {from:5, to:4})
-    ht.remove('cigar')
-    expect(ht.contains('cigar')).to.equal(false)
+    ht.remove('shigar')
+    expect(ht.contains('shigar')).to.equal(false)
   })
 
   it('size(): returns the number of elements in the hash table', () => {
     ht.put('asuh','dood')
     ht.put('hoi','mate')
-    ht.put('cigar','waffle')
-    ht.put('smoke','pancake')
+    ht.put('shigar','waffle')
+    ht.put('shmoke','pancake')
     ht.put('pipe', 'bagel')
     expect(ht.size()).to.eql(5)
   })
--- src/hash_table.js
+++ src/hash_table.js
@@ -1,134 +1,91 @@
-export default class HashTable {
-  constructor() {
-    this.currentSize = 0
-    this.storageMax = 4
-    this.storage = []
-  }
-  put(key, value) {
-    let hash = HashTable.hash(key) % this.storageMax
-
-    if(this.storage[hash]) {
-      this.storage[hash].insertNode(key, value)
-    } else {
-      this.storage[hash] = new Bucket(new Node(key, value))
+class hashNode {
+    constructor(key, value) {
+        this.key = key
+        this.value = value
+        this.next = null
     }
-
-    this.currentSize += 1
-    // if(this.currentSize / this.storageMax >= 0.75) {
-    //   this.storageMax *= 2
-    // } TODO: fix resizing
-  } // adds a key-value pair to the hash table.
-  get(key) {
-    let hash = HashTable.hash(key) % this.storageMax
-    let value = this.storage[hash].getNode(key)
-    return value || "There is no data associated with this key"
-  } // returns the data associated with key.
-  contains(key) {
-    let hash = HashTable.hash(key) % this.storageMax
-    return this.storage[hash]? this.storage[hash].getNode(key) !== null : false
-  } // returns true if the hash table contains the key.
-  iterate(callback) {
-    this.storage.forEach(linkedList => linkedList.map(callback))
-  } // takes a callback function and passes it each key and value in sequence.
-  remove(key) {
-    let hash = HashTable.hash(key) % this.storageMax
-    this.storage[hash].removeNode(key)
-    this.currentSize -= 1
-    //TODO: resize down by half when utilization drops below 25%
-  } // removes a key-value pair by key.
-  size() {
-    return this.currentSize
-  } // returns the number of key-value pairs in the hash table.
 }
 
-let hashFunction = (string) => {
-  let hash = 0
-  let char
-  if(string.length === 0) {
-    return hash
-  }
-  let charArray = string.split('')
-  charArray.forEach( (element) => {
-    char = element.charCodeAt(0)
-    hash = ((hash << 5) - hash) + char
-    hash = hash & hash
-  })
-  return hash
+export default class HashTable {
+    constructor() {
+        this.currentSize = 0
+        this.storageMax = 64
+        this.storage = []
+    }
+
+    put(key, value) {
+        let hash = HashTable.hash(key) % this.storageMax
+        this.storage[hash] = new hashNode(key, value)
+
+        this.currentSize += 1
+        if (this.currentSize / this.storageMax >= 0.75) {
+            this.storageMax *= 2
+            this.resize()
+        }
+    }
+
+    get(key) {
+        let hash = HashTable.hash(key) % this.storageMax
+        return this.storage[hash].value
+    }
+
+    contains(key) {
+        let hash = HashTable.hash(key) % this.storageMax
+        return this.storage[hash] ? this.storage[hash].key === key : false
+    }
+
+    iterate(callback) {
+        this.storage.forEach(hashNode => {
+            if (hashNode) {
+                callback(hashNode.key, hashNode.value)
+            }
+        })
+    }
+    remove(key) {
+        let hash = HashTable.hash(key) % this.storageMax
+        this.storage[hash] = undefined
+
+        this.currentSize -= 1
+
+        if (this.currentSize / this.storageMax <= 0.25) {
+            this.storageMax /= 2
+            this.resize()
+        }
+    }
+    size() {
+        return this.currentSize
+    }
+
+    resize() {
+        let oldStorage = this.storage.slice()
+        this.storage = []
+
+        oldStorage.forEach(hashNode => {
+            if (hashNode) {
+                let hash = HashTable.hash(hashNode.key) % this.storageMax
+                this.storage[hash] = hashNode
+            }
+        })
+
+    }
+
 }
 
 HashTable.hash = (string) => {
-  return hashFunction(string)
+    return hashFunction(string)
 }
 
-class Node {
-  constructor(key, value) {
-    this.key = key
-    this.value = value
-    this.next = null
-  }
-}
-
-class Bucket {
-  constructor(node = null) {
-    this.root = node
-  }
-
-  getHeadNode() {
-    return this.root
-  }
-
-  setHeadNode(node) {
-    node.next = this.root
-    this.root = node
-  }
-
-  getNode(key) {
-    let matchedNode = findNode(key, this.getHeadNode())
-    return matchedNode ? matchedNode.value : null
-  }
-
-  insertNode(key, value) {
-    this.setHeadNode(new Node(key,value))
-  }
-
-  removeNode(key) {
-    let matchedNode = findPreNode(key, this.getHeadNode())
-    if(matchedNode) {
-      matchedNode.next = matchedNode.next? matchedNode.next.next : null
+let hashFunction = (string) => {
+    let hash = 0
+    let char
+    if (string.length === 0) {
+        return hash
     }
-  }
-
-  getSize() {
-    let size = 0
-    let currentNode = this.getHeadNode()
-    while(currentNode) {
-      size += 1
-      currentNode = currentNode.next
-    }
-    return size
-  }
-
-  map(callback) {
-    let currentNode = this.getHeadNode()
-    while(currentNode) {
-      callback(currentNode.key, currentNode.value)
-      currentNode = currentNode.next
-    }
-  }
-}
-
-let findNode = (key, startNode) => {
-  let currentNode = startNode
-  while(currentNode && currentNode.key !== key) {
-    currentNode = currentNode.next
-  }
-  return currentNode
-}
-
-let findPreNode = (key, startNode) => {
-  let currentNode = startNode
-  while(currentNode.next && currentNode.next.key !== key) {
-    currentNode = currentNode.next
-  }
-  return currentNode
+    let charArray = string.split('')
+    charArray.forEach((element) => {
+        char = element.charCodeAt(0)
+        hash = ((hash << 5) - hash) + char
+        hash = hash & hash
+    })
+    return hash
 }
