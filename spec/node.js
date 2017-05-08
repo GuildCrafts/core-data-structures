d2e366681379903265f0fc730544ffab2565385e
Aileen <asantos3026@gmail.com>
2017-05-08 15:16:44 -0700

    WIP test for setnext and node pass

---
+++ spec/node.js
@@ -0,0 +1,21 @@
+import chai, { expect } from 'chai'
+import chaiChange from 'chai-change'
+import Node from '../src/node'
+
+chai.use(chaiChange)
+
+describe.only('Node', () => {
+
+  it('is a function', () => {
+    const node = new Node(15)
+    expect(Node).to.be.a('function')
+  })
+
+  it('changes the reference to the next node and returns the original node', () => {
+    const node = new Node()
+    node.setNext(14)
+    expect(node.getNext().data).to.eql(14)
+  })
+
+})
+
---
+++ src/node.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor() {
+    this.data = null
+    this.next = null
+  }
+
+  getData(data) {
+    return data
+  }
+
+  setNext(data) {
+    const currentNode = new Node()
+    currentNode.data = data
+      this.next = currentNode
+      return this
+    }
+
+  getNext() {
+    if (this.next === null) {
+    }
+    return this.next
+  }
+
+}
+
+// const node = new Node()
+// node.data = 15
+// console.log(node)
+// node.setNext(14)
+// console.log(node)
+// console.log(node.getNext().data)
+
+export default Node
