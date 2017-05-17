e2cc86263df023c291c104a79e9ec1e21720cf49
Fodé Diop <fodesdiop@gmail.com>
2017-05-16 22:50:15 -0700

    reset linked list tests

--- src/linkedList.js
+++ src/linkedList.js
@@ -10,23 +10,6 @@ export default class LinkedList {
     this.length = 0
   }
 
-
-  // Returns the first node in the list
-  getHead(){
-      return this.head
-  }
-
-  // Returns the last node in the list
-  getTail(){
-      return this.tail
-  }
-
-  // Returns the size of the list (number of nodes)
-  size(){
-      return this.length
-  }
-
-  // Inserts a node (with the provided data) to the tail of the list
   insert(nodeData){
       let node = new Node({data: 'nodeData'})
       if (this.head === null){
@@ -43,50 +26,4 @@ export default class LinkedList {
       return node
   }
 
-  // Determines whether or not the list contains the provided data
-  contains(data){
-
-  }
-
-  // Returns the first node containing the provided data, or -1 if not found
-  find(data){
-
-  }
-
-
-
-  // Inserts a node (with the provided data) to the head of the list
-  insertFirst(data){
-
-  }
-
-  // Inserts a node (with data "apples") before the first node containing "bananas"
-  insertBefore(data, index){
-
-  }
-
-  // Inserts a node (with data "bananas") after the first node containing "apples"
-  insertAfter(data, inex){
-
-  }
-
-  // Removes the tail node from the list
-  remove(){
-
-  }
-
-  // Removes the head node from the list
-  removeFirst(){
-
-  }
-
-  // Determines if the list is empty or not
-  isEmpty(){
-
-  }
-
-   // Clears the list of all nodes/data
-   clear(){
-
-   }
 }
