2610d76e0adec718a91c110ffd8db4598bedc110
Jonathan Pool <pool@stulta.com>
2017-05-18 14:17:23 -0700

    Renamed Set to PowerSet to avoid conflict with JS Set built-in. Finished developing, testing, and debugging PowerSet.

---
+++ src/power_set.js
@@ -0,0 +1,101 @@
+'use strict';
+
+/*
+  Class declaration for PowerSet, with a default constructor, and export
+  statement making that object the default export from this module.
+*/
+export default class PowerSet {
+  constructor(iterableObject) {
+    this.pSet = new Set(iterableObject);
+  }
+
+  // INTERROGATION METHODS
+
+  // Returns whether the powerSet is empty.
+  isEmpty() {
+    return this.pSet.size === 0;
+  }
+
+  // Returns whether the powerSet contains the specified element.
+  contains(value) {
+    return this.pSet.has(value);
+  }
+
+  // Returns the count of elements.
+  size() {
+    return this.pSet.size;
+  }
+
+  // DUAL-SET INTERROGATION METHODS
+
+  // Returns the union of this and another powerSet.
+  union(otherPowerSet) {
+    const newPowerSet = new PowerSet(this.pSet);
+    const addToThis = function(element, key, sourceSet) {
+      this.add(element);
+    };
+    otherPowerSet.pSet.forEach(addToThis, newPowerSet.pSet);
+    return newPowerSet;
+  }
+
+  // Returns the intersection of this and another powerSet.
+  intersect(otherPowerSet) {
+    const newPowerSet = new PowerSet();
+    const addToThis = function(element, key, thisSet) {
+      if (otherPowerSet.contains(element)) {
+        this.add(element);
+      }
+    };
+    this.pSet.forEach(addToThis, newPowerSet.pSet);
+    return newPowerSet;
+  }
+
+  // Returns the difference of this from another powerSet.
+  difference(otherPowerSet) {
+    const newPowerSet = new PowerSet();
+    const addToThis = function(element, key, thisSet) {
+      if (! otherPowerSet.contains(element)) {
+        this.add(element);
+      }
+    };
+    this.pSet.forEach(addToThis, newPowerSet.pSet);
+    return newPowerSet;
+  }
+
+  // Returns whether this is a subset of another powerSet.
+  isSubset(otherPowerSet) {
+    for (element of this.pSet) {
+      if (! otherPowerSet.contains(element)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // MANIPULATION METHODS
+
+  // Adds a specified element, if not already in the PowerSet.
+  add(value) {
+    if (value !== undefined) {
+      this.pSet.add(value);
+    }
+  }
+
+  // Removes a specified element, if in the PowerSet.
+  remove(value) {
+    this.pSet.delete(value);
+  }
+
+  // Returns a copy of the PowerSet.
+  clone() {
+    return new PowerSet(this.pSet);
+  }
+
+  // ELEMENT ITERATION METHODS
+
+  // Executes a specified function on each element.
+  forEach(fn) {
+    this.pSet.forEach(fn, this.pSet);
+  }
+
+}
