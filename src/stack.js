43f02eb3a40d7237847b125e8965ce58812745c5
je-lopez <je.lopez@me.com>
2018-04-24 14:58:44 -0700

    create stack data structure and add tests

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,35 @@
 'use strict'
 
+import Node from './node';
+// L I F O
 export default class Stack {
-  // your code here
+  constructor() {
+    this.top = null;
+    this.totalLength = 0;
+  }
+
+  isEmpty() {
+    return this.top === null;
+  }
+
+  length() {
+    return this.totalLength;
+  }
+
+  peek() {
+    return this.top;
+  }
+
+  push(data) {
+    this.totalLength++;
+    const node = new Node({ data, next: this.top });
+    this.top = node;
+  }
+
+  pop() {
+    this.totalLength -= (this.totalLength === 0 ? 0 : 1);
+    const node = this.top;
+    this.top = node.getNext();
+    return node;
+  }
 }
